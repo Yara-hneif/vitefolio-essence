93e1140ebe3a8bfc9d9d7f3817bbbd824ed9288c
Yara Hneif <yarahneif131@gmail.com>
2025-08-02 16:26:40 +0300

    feat: define project routes for GET /api/projects

--- server/api/projects.js
+++ server/api/projects.js
@@ -1,11 +1,17 @@
 //  This function retrieves all projects from the database.
 //  It uses the PostgreSQL connection pool to execute a query and return the results.
 import express from "express";
-import { getAllProjects } from "../controllers/projectsController.js";
+import {
+  getAllProjects,
+  getProjectBySlug,
+} from "../controllers/projectsController.js";
 
 const router = express.Router();
 
 // GET /api/projects
 router.get("/", getAllProjects);
+// GET /api/projects/:slug
+router.get("/:slug", getProjectBySlug);
+
 
 export default router;
