5719fdfc6008fb35ee62d909b297492aeecd45eb
Yara Hneif <yarahneif131@gmail.com>
2025-08-02 16:25:56 +0300

    feat: implement getAllProjectsFromDB to fetch published projects

--- server/models/projectModel.js
+++ server/models/projectModel.js
@@ -1,6 +1,16 @@
 import pool from "../config/db.js";
 
-export const getAllProjectsFromDB = async () => {
-  const result = await pool.query("SELECT * FROM projects ORDER BY id ASC");
+export const getAllProjectsFromDB = async (userId) => {
+  const result = await pool.query(
+    `SELECT * FROM projects WHERE user_id = $1 AND status = 'published' ORDER BY created_at DESC`,
+    [userId]
+  );
   return result.rows;
+};
+export const getProjectBySlugFromDB = async (userId, slug) => {
+  const result = await pool.query(
+    `SELECT * FROM projects WHERE user_id = $1 AND slug = $2 AND status = 'published'`,
+    [userId, slug]
+  );
+  return result.rows[0];
 };
