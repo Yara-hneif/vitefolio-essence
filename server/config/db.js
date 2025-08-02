caddbc14b7a6b18d72d3fe58b4db09f0b83a51cc
Yara Hneif <yarahneif131@gmail.com>
2025-08-02 16:25:36 +0300

    chore: setup PostgreSQL connection with environment variables

--- server/config/db.js
+++ server/config/db.js
@@ -11,7 +11,7 @@ const pool = new Pool({
   user: process.env.DB_USER,          
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWOR,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
 
